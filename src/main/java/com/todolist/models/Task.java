169724ae5efafa3b0ce177eb44071e245f3d959d
Mateusz <bednarczyk.matt@gmail.com>
2023-04-06 16:38:25 +0200

    feature #2 implemented

--- src/main/java/com/todolist/models/Task.java
+++ src/main/java/com/todolist/models/Task.java
@@ -4,31 +4,31 @@ public class Task {
     private Long id;
     private String title;
     private String description;
+    private String color;
 
-    public Task(String title,String description) {
+    public Task(String title,String description,String color) {
         this.title = title;
         this.description = description;
+        this.color = color;
     }
+    public String getColor() { return color; }
     public String getTitle(){
         return title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Long getId(){
         return id;
     }
-
     public String toString() {
         return  " id: " + id +
                 " title: " + title +
-                " description: " + description;
+                " description: " + description +
+                " color: " + color;
     }
 
 }

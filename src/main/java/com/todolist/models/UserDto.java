bd4afeda490e91cedffb7be7559c75aa631c7889
Mateusz <bednarczyk.matt@gmail.com>
2023-06-27 11:08:28 +0200

    UserDto as immutable class

--- src/main/java/com/todolist/models/UserDto.java
+++ src/main/java/com/todolist/models/UserDto.java
@@ -1,11 +1,11 @@
 package com.todolist.models;
 
-public class UserDto {
+public final class UserDto {
 
-    private Long id;
-    private String name;
-    private String surname;
-    private String mail;
+    private final Long id;
+    private final String name;
+    private final String surname;
+    private final String mail;
 
     public UserDto(Long id, String name, String surname, String mail) {
         this.id = id;
@@ -17,32 +17,13 @@ public class UserDto {
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSurname() {
         return surname;
     }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
     public String getMail() {
         return mail;
     }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 }

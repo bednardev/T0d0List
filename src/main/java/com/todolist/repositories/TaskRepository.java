be99639ba826cd6cffe86a6751b5d485212bc335
Mateusz <bednarczyk.matt@gmail.com>
2023-06-30 13:17:36 +0200

    getTasks method rebuilt in service to cover specification

--- src/main/java/com/todolist/repositories/TaskRepository.java
+++ src/main/java/com/todolist/repositories/TaskRepository.java
@@ -1,17 +1,10 @@
 package com.todolist.repositories;
 
-import com.todolist.models.Color;
 import com.todolist.models.Task;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface TaskRepository extends CrudRepository<Task, Long>, JpaSpecificationExecutor {
 
-    @Query("SELECT task FROM Task task WHERE task.title like %:title% AND task.color = :color")
-    List<Task> findTasksByTitleAndColor(@Param("title") String title, @Param("color") Color color);
 }
 

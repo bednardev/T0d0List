0542fccf4978e1d027737f22cc6af2ec09b61023
bednardev <129054448+bednardev@users.noreply.github.com>
2023-07-10 11:51:49 +0200

    feature/26 - sorting endpoint implementation (#37)

--- src/main/java/com/todolist/controllers/TaskController.java
+++ src/main/java/com/todolist/controllers/TaskController.java
@@ -3,6 +3,8 @@ package com.todolist.controllers;
 import com.todolist.models.TaskDto;
 import com.todolist.services.TaskService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
@@ -27,6 +29,14 @@ public class TaskController {
     public List<TaskDto> getTasks(@RequestParam(value = "color", required = false) String color,
                                   @RequestParam(value = "title", required = false) String title) {
         return taskService.getTasks(color, title);
+    }
+
+    @GetMapping("/page")
+    public Page<TaskDto> getTasksAsPage(@RequestParam(value ="pageNo", required = false, defaultValue = "0") Integer pageNumber,
+                                        @RequestParam(value = "size", required = false, defaultValue = "1") Integer pageSize,
+                                        @RequestParam(value = "sort", required = false, defaultValue = "title") String sort,
+                                        @RequestParam(value = "direction", required = false, defaultValue = "ASC") Sort.Direction direction) throws IllegalArgumentException {
+        return taskService.getTasksAsPage(pageNumber, pageSize, sort, direction);
     }
 
     @PutMapping("{id}")
--- src/main/java/com/todolist/repositories/TaskRepository.java
+++ src/main/java/com/todolist/repositories/TaskRepository.java
@@ -3,8 +3,10 @@ package com.todolist.repositories;
 import com.todolist.models.Task;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TaskRepository extends CrudRepository<Task, Long>, JpaSpecificationExecutor {
+public interface TaskRepository extends CrudRepository<Task, Long>, JpaSpecificationExecutor, PagingAndSortingRepository<Task, Long>
+{
 
 }
 

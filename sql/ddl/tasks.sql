8e872fcff4bcf8d522bfdbf0919fc2a9cf8bbff8
Mateusz <bednarczyk.matt@gmail.com>
2023-09-13 19:50:31 +0200

    task sql/ddl update

--- sql/ddl/tasks.sql
+++ sql/ddl/tasks.sql
@@ -5,7 +5,14 @@ create table tasks
     Title         varchar(50)  not null,
     Description   varchar(200) null,
     Color         varchar(5)   not null,
-    CreatedAt     date         null,
-    LastUpdatedAt date         null
+    CreatedAt     date         not null,
+    LastUpdatedAt date         null,
+    Status        varchar(15)  not null,
+    UserId        int          not null,
+    constraint tasks_ibfk_1
+        foreign key (UserId) references users (ID)
 );
+
+create index UserId
+    on tasks (UserId);
 

3ac83cb9f97ca8dfdf9aaa7e416e5fc0238d9730
Mateusz <bednarczyk.matt@gmail.com>
2023-09-15 14:21:46 +0200

    revert of rebase

--- src/main/java/com/todolist/models/UserDto.java
+++ src/main/java/com/todolist/models/UserDto.java
@@ -1,8 +1,19 @@
 package com.todolist.models;
 
-public record UserDto
-         (Long id,
-         String name,
-         String surname,
-         String mail) {
+import lombok.Getter;
+
+@Getter
+public final class UserDto {
+
+    private final Long id;
+    private final String name;
+    private final String surname;
+    private final String mail;
+
+    public UserDto(Long id, String name, String surname, String mail) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+    }
 }
